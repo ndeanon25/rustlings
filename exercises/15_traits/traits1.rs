f9dd3e78b4a1ed4cec077b69eced0f128bd94ee8
ndeanon25 <146136622+ndeanon25@users.noreply.github.com>
2024-06-07 10:26:34 +0000

    SC HW 06

--- exercises/15_traits/traits1.rs
+++ exercises/15_traits/traits1.rs
@@ -5,9 +5,9 @@
 // which appends "Bar" to any object implementing this trait.
 //
 // Execute `rustlings hint traits1` or use the `hint` watch subcommand for a
-// hint.
+// hint
 
-// I AM NOT DONE
+
 
 trait AppendBar {
     fn append_bar(self) -> Self;
@@ -15,6 +15,9 @@ trait AppendBar {
 
 impl AppendBar for String {
     // TODO: Implement `AppendBar` for type `String`.
+    fn append_bar(self) -> Self {
+        self + "Bar"
+    }
 }
 
 fn main() {
